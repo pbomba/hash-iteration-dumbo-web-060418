7a7f9431fac5e689e34a7641e9051035f0ef8af5
Paul Bomba <paul@paulbomba.com>
2018-05-30 18:53:37 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,13 +7,12 @@
 # }
 
 def select_winner(passengers)
-    winner = ""
-    passengers.each do |suite, name|
-      if suite == :suite_a && name.start_with?("A")
-        winner = name
-      end
+winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
     end
-     
-    winner  
-
+  end
+  winner
 end
+
